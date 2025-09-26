982fea227e025997fc3a6f653a7d000a8048d7fc
MrBigBrainy <mrbigbrainy@gmail.com>
2025-09-26 17:14:49 +0700

    update

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Outlet, useNavigation } from 'react-router'
 import { Link } from 'react-router';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./Layout"
 import HomePage from "./HomePage"
-import PostPage from "./PostPage";
-import { fetchData } from "./api/fetchData";
+
 
 const router = createBrowserRouter([
     {
@@ -10,7 +9,7 @@ const router = createBrowserRouter([
         element: <Layout />,
         errorElement: <h1>Page Not Found</h1>,
         children: [{ index: true, element: <HomePage /> },
-        { path: "post", element: <PostPage/>, loader: fetchData }]
+      ]
     }
 ], {
     basename: '/Test-React-Router'
